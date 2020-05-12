8638a633bae3aaab36935f0d199dd81cd90d92d1
yasminleski <48722794+yasminleski@users.noreply.github.com>
2020-05-11 22:27:26 -0300

    Create formularioCadastroUsuario.php

---
+++ formularioCadastroUsuario.php
@@ -0,0 +1,18 @@
+<html >
+  <head> 
+      <meta charset="utf-8">
+  </head>
+   <body>
+      <form class="" action="controlerUsuario.php" method="post">
+     <label for="">Nome:</label>
+      <input type="text" name="nome" value=""> <br>
+      <label for="">Email: </label>
+      <input type="email" name="email" value=""> <br>
+     <label for="">Login:</label>
+      <input type="text" name="login" value=""> <br>
+      <label for="">Senha:</label>
+      <input type="password" name="senha" value=""> <br>
+      <input type="submit" name="" value="Cadastrar"> 
+    </form>
+  </body>
+</html>
